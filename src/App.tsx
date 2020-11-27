3bf18c4771a425f5d1f2bdda3c68ce3fc0cf0ca2
Danko Lučić <ld.is.a.git@gmail.com>
2020-11-27 19:05:35 +0100

    use styled-components for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 import { Typography } from "@material-ui/core";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppContainer>
+      <Header>
         <Typography component="h1">Pew Pew Pew</Typography>
-      </header>
-      <main>
-
-      </main>
-    </div>
+      </Header>
+      <main></main>
+    </AppContainer>
   );
 }
 
