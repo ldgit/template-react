0f8e621cd86bcf7633b2f3e95f84dcc982238d0c
Danko Lučić <ld.is.a.git@gmail.com>
2020-11-27 21:09:54 +0100

    install prettier, fix code style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import styled from "styled-components";
-
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import styled from 'styled-components';
 
 const Title = styled(Typography)`
   flex-grow: 1;
